5439ea391dd0c8adddf872759c6a656bcb310948
Raja <rdas6313@gmail.com>
2018-05-06 16:52:17 +0530

    signUp Completed

--- app/src/main/java/com/example/rdas6313/quizz/SignUpActivity.java
+++ app/src/main/java/com/example/rdas6313/quizz/SignUpActivity.java
@@ -1,57 +1,117 @@
 package com.example.rdas6313.quizz;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.rdas6313.quizz.Interfaces.PresenterCallBack;
 import com.example.rdas6313.quizz.Interfaces.PresenterConnection;
 import com.example.rdas6313.quizz.Presenters.LoginAndSignUp;
 
+import java.util.regex.Pattern;
+
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener,PresenterCallBack{
 
-    private EditText nameView,emailView,passwordView;
+    private EditText nameView,emailView,passwordView,confirmPasswordView;
     private Button signupBtn;
-    PresenterConnection connection;
+    private CoordinatorLayout coordinatorLayout;
+    private PresenterConnection connection;
+    private Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         connection = new LoginAndSignUp();
+        coordinatorLayout = (CoordinatorLayout)findViewById(R.id.coordinatorLayout);
         nameView = (EditText)findViewById(R.id.name);
         emailView = (EditText)findViewById(R.id.email);
         passwordView = (EditText)findViewById(R.id.password);
+        confirmPasswordView = (EditText)findViewById(R.id.confirm_password);
         signupBtn = (Button)findViewById(R.id.signup);
         signupBtn.setOnClickListener(this);
+        dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.login_sign_up_dialog);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.setCancelable(false);
+        TextView textView = (TextView)dialog.findViewById(R.id.dialogText);
+        textView.setText(R.string.dialog_sign_up);
     }
 
     private void signUp(){
         String email = emailView.getText().toString();
         String name = nameView.getText().toString();
         String password = passwordView.getText().toString();
+        String confirm_password = confirmPasswordView.getText().toString();
         boolean isError = false;
         if(TextUtils.isEmpty(email)){
-            emailView.setError("this Field can't be empty");
+            emailView.setError(getString(R.string.empty_editText_Error));
             isError = true;
+        }else{
+            String regx = "\\S+@\\S+\\.\\S+";
+            if(!Pattern.matches(regx,email)){
+                emailView.setError(getString(R.string.email_formatting_error));
+                isError = true;
+            }
         }
         if(TextUtils.isEmpty(name)){
-            nameView.setError("this Field can't be empty");
+            nameView.setError(getString(R.string.empty_editText_Error));
             isError = true;
         }
         if(TextUtils.isEmpty(password)){
-            passwordView.setError("this Field can't be empty");
+            passwordView.setError(getString(R.string.empty_editText_Error));
+            isError = true;
+        }
+        if(TextUtils.isEmpty(confirm_password)){
+            confirmPasswordView.setError(getString(R.string.empty_editText_Error));
             isError = true;
         }
         if(isError)
             return;
-        if(connection != null)
-            connection.signUpUser(email,password,name,this);
+        else if(!password.equals(confirm_password)){
+            Snackbar.make(coordinatorLayout, R.string.passoword_not_matched,Snackbar.LENGTH_SHORT).show();
+            return;
+        }
+        if(connection != null) {
+            changeView(1);
+            connection.signUpUser(email, password, name, this);
+        }else{
+            Snackbar.make(coordinatorLayout, getString(R.string.Internal_Error),Snackbar.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void showDialog(){
+       if(dialog != null)
+            dialog.show();
+    }
+
+    private void cancelDialog(){
+        if(dialog != null)
+            dialog.dismiss();
+    }
+
+    private void changeView(int v){
+        switch (v){
+            case 1:
+                showDialog();
+                break;
+            case 2:
+                cancelDialog();
+                break;
+        }
     }
 
     @Override
@@ -65,10 +125,12 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
     @Override
     public void onSignUpResponse(boolean isError, String msg) {
+        changeView(2);
         if(isError){
-            Toast.makeText(this,"Sign Up Failed",Toast.LENGTH_SHORT).show();
+            Snackbar.make(coordinatorLayout,msg,Snackbar.LENGTH_SHORT).show();
+            //Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
         }else{
-            Toast.makeText(this,"Sign Up Successfully",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(this,MainActivity.class);
             startActivity(intent);
             finish();
