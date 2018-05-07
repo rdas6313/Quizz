9f38e23a8bb97a30cad94e0a471eb1f92ed60e00
Raja <rdas6313@gmail.com>
2018-05-07 12:51:44 +0530

    worked on login page progressbar and email

--- app/src/main/java/com/example/rdas6313/quizz/LoginActivity.java
+++ app/src/main/java/com/example/rdas6313/quizz/LoginActivity.java
@@ -1,10 +1,14 @@
 package com.example.rdas6313.quizz;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,17 +18,22 @@ import com.example.rdas6313.quizz.Interfaces.PresenterCallBack;
 import com.example.rdas6313.quizz.Interfaces.PresenterConnection;
 import com.example.rdas6313.quizz.Presenters.LoginAndSignUp;
 
+import java.util.regex.Pattern;
+
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener,PresenterCallBack{
 
     private PresenterConnection connection;
     private Button loginBtn;
     private EditText emailView,passwordView;
     private TextView forgotPasswordView,signUpBtn;
+    private CoordinatorLayout coordinatorLayout;
+    private Dialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         connection = new LoginAndSignUp();
+        coordinatorLayout = (CoordinatorLayout)findViewById(R.id.coordinatorLayout);
         signUpBtn = (TextView) findViewById(R.id.signup);
         loginBtn = (Button)findViewById(R.id.login);
         emailView = (EditText)findViewById(R.id.email);
@@ -33,6 +42,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         loginBtn.setOnClickListener(this);
         forgotPasswordView = (TextView)findViewById(R.id.forgotPassword);
         forgotPasswordView.setOnClickListener(this);
+        dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.setContentView(R.layout.login_sign_up_dialog);
+        TextView textView = (TextView)dialog.findViewById(R.id.dialogText);
+        textView.setText(R.string.login_dialog_text);
     }
 
     @Override
@@ -60,17 +76,27 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         String password = passwordView.getText().toString();
         boolean isError = false;
         if(TextUtils.isEmpty(email)){
-            emailView.setError("this field can't be empty");
+            emailView.setError(getString(R.string.empty_editText_Error));
             isError = true;
+        }else{
+            String regx = "\\S+@\\S+\\.\\S+";
+            if(!Pattern.matches(regx,email)){
+                emailView.setError(getString(R.string.email_formatting_error));
+                isError = true;
+            }
         }
         if(TextUtils.isEmpty(password)){
-            passwordView.setError("this field can't be empty");
+            passwordView.setError(getString(R.string.empty_editText_Error));
             isError = true;
         }
         if(isError)
             return;
-        if(connection != null)
-            connection.loginUser(email,password,this);
+        if(connection != null) {
+            showDialog();
+            connection.loginUser(email, password, this);
+        }else{
+            Snackbar.make(coordinatorLayout,getString(R.string.Internal_Error),Snackbar.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -88,10 +114,22 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
+    private void showDialog(){
+        if(dialog != null)
+            dialog.show();
+    }
+
+    private void dismissDialog(){
+        if(dialog != null){
+            dialog.dismiss();
+        }
+    }
+
     @Override
     public void onLoginResponse(boolean isError, String msg) {
+        dismissDialog();
         if(isError){
-            Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+            Snackbar.make(coordinatorLayout,msg,Snackbar.LENGTH_SHORT).show();
         }else{
             Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(this,MainActivity.class);
